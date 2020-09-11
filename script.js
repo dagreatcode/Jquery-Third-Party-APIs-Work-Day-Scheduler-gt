ea521d2bde696594b65a6caac27564887695c6a6
dagreatcode <dagreatcode@gmail.com>
2020-09-11 18:20:38 -0400

    maybe im getting it

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var m = moment();
+m.format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
+m.format('dddd');                    // Friday
+m.format("MMM Do YY");               // Sep 11th 20
+m.format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+m.format(); 
+console.log(moment);
 // // Just a Reach
 // var headerJumbotron = "jumbotron"
 // var display3 = "display-3"
@@ -26,14 +33,11 @@ $.ajax({
     method: "GET"
 }).then (function (response) {
     console.log(response);
-    console.log(response.runtime)
+    $("display-3").text(JSON.stringify(response));
 });
-<script>
-    moment().format();
-</script>
 
-moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
-moment().format('dddd');                    // Friday
-moment().format("MMM Do YY");               // Sep 11th 20
-moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
-moment().format();                          // 2020-09-11T17:54:06-04:00
+// moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
+// moment().format('dddd');                    // Friday
+// moment().format("MMM Do YY");               // Sep 11th 20
+// moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+// moment().format();                          // 2020-09-11T17:54:06-04:00
