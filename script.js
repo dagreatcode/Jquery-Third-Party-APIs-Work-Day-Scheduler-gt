2b8c8e9828982a9498919ca881182f616a46d267
dagreatcode <dagreatcode@gmail.com>
2020-09-11 16:40:32 -0400

    made a change in the var

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var headerJumbotron = "jumbotron"
-var display3 = "display-3"
-var lead = "lead"
-var currentDay = "currentDay"
-var container = "container"
+// // Just a Reach
+// var headerJumbotron = "jumbotron"
+// var display3 = "display-3"
+// var lead = "lead"
+// var currentDay = "currentDay"
+// var timeBlock = "container"
+
+//
+var headerJumbotronEl = $("jumbotron");
+var display3El = $("display-3");
+var leadEl = $("lead");
+var currentDayEl = $("currentDay");
+var timeBlockEl = $("container");
+
+//
+function timeClock (moment) {
+    if 
+}
