4def0fb929c8f9f32dd2e3042ce6c63295932e35
dagreatcode <dagreatcode@gmail.com>
2020-09-11 23:12:37 -0400

    playing in my google chrome tools...

---
+++ junk.js
@@ -0,0 +1,56 @@
+
+// var a = moment().format('MMMM Do YYYY, h:mm:ss a');
+// document.getElementById("currentDay").innerHTML = a;
+// Waits Until The HTML has loaded. Wait4DocReady
+
+
+// //     //Converting the tags
+//     var headerJumbotronEl = $(".jumbotron");
+//     var display3El = $(".display-3");
+//     var leadEl = $(".lead");
+//     var currentDayEl = $("#currentDay");
+//     var timeBlockEl = $(".container");
+//     console.log(timeBlockEl);
+
+//     display3El.on("click", function() {
+        
+//     })
+// });
+
+// m.format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
+// m.format('dddd');                    // Friday
+// m.format("MMM Do YY");               // Sep 11th 20
+// m.format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+// m.format(); 
+// console.log(a);
+// // Just a Reach
+// var headerJumbotron = "jumbotron"
+// var display3 = "display-3"
+// var lead = "lead"
+// var currentDay = "currentDay"
+// var timeBlock = "container"
+// var queryURL = ""
+// $(document).ready(function () {
+// //     //Converting the tags
+// var headerJumbotronEl = $(".jumbotron");
+// var display3El = $(".display-3");
+// var leadEl = $(".lead");
+// var currentDayEl = $("#currentDay");
+// var timeBlockEl = $(".container");
+
+    
+//     //
+
+// })
+// //
+
+// function timeClock (moment) {
+//     if 
+// }
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then (function (response) {
+//     console.log(response);
+//     $("display-3").text(JSON.stringify(response));
+// });
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// var a = moment().format('MMMM Do YYYY, h:mm:ss a');
-// document.getElementById("currentDay").innerHTML = a;
 $(document).ready(function () {
+    var m = moment();
+    console.log(m);
+    // moment().format();
+    console.log(m.format("dddd MMM Mo YYYY"));
+    console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
+// //     //Converting the tags
+// var headerJumbotronEl = $(".jumbotron");
+// var display3El = $(".display-3");
+// var leadEl = $(".lead");
+});
+    var currentDayEl = $("#currentDay");
+    var timeBlockEl = $(".container");
 
-var m = moment();
-console.log(m);
-// moment().format();
-console.log(m.format("dddd MMM Mo YYYY"));
-console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
+    currentDayEl.text(m.format('MMMM Do YYYY')); // September 11th 2020, 10:46:49 pm
+    timeBlockEl.text(m.format('h:mm:ss a'));
+
+    
+// moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 10:46:49 pm
+// moment().format('dddd');                    // Friday
+// moment().format("MMM Do YY");               // Sep 11th 20
+// moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+// moment().format();                          // 2020-09-11T22:46:49-04:00
