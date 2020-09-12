10f9be4bd077327883b7fdb3785f66ccacaf2582
dagreatcode <dagreatcode@gmail.com>
2020-09-11 22:42:14 -0400

    still trying to figure out moment smh

--- script.js
+++ script.js
@@ -1,48 +1,9 @@
 // var a = moment().format('MMMM Do YYYY, h:mm:ss a');
 // document.getElementById("currentDay").innerHTML = a;
+$(document).ready(function () {
+
 var m = moment();
-moment().format();
-console.log(m)
-// m.format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
-// m.format('dddd');                    // Friday
-// m.format("MMM Do YY");               // Sep 11th 20
-// m.format('YYYY [escaped] YYYY');     // 2020 escaped 2020
-// m.format(); 
-// console.log(a);
-// // Just a Reach
-// var headerJumbotron = "jumbotron"
-// var display3 = "display-3"
-// var lead = "lead"
-// var currentDay = "currentDay"
-// var timeBlock = "container"
-// var queryURL = ""
-// $(document).ready(function () {
-// //     //Converting the tags
-// var headerJumbotronEl = $(".jumbotron");
-// var display3El = $(".display-3");
-// var leadEl = $(".lead");
-// var currentDayEl = $("#currentDay");
-// var timeBlockEl = $(".container");
-
-    
-//     //
-
-// })
-// //
-
-// function timeClock (moment) {
-//     if 
-// }
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then (function (response) {
-//     console.log(response);
-//     $("display-3").text(JSON.stringify(response));
-// });
-
-// moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 5:54:06 pm
-// moment().format('dddd');                    // Friday
-// moment().format("MMM Do YY");               // Sep 11th 20
-// moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
-// moment().format();                          
+console.log(m);
+// moment().format();
+console.log(m.format("dddd MMM Mo YYYY"));
+console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
