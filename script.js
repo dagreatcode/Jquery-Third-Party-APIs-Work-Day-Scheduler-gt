a7c18d7af0e0bbacd192324aad3a348c048e7b95
dagreatcode <dagreatcode@gmail.com>
2020-09-11 23:23:29 -0400

    finally see something in the chrome tools, I see moment finally

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
     var m = moment();
     console.log(m);
     // moment().format();
-    console.log(m.format("dddd MMM Mo YYYY"));
-    console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
+    // console.log(m.format("dddd MMM Mo YYYY"));
+    // console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
 // //     //Converting the tags
 // var headerJumbotronEl = $(".jumbotron");
 // var display3El = $(".display-3");
 // var leadEl = $(".lead");
 });
-    var currentDayEl = $("#currentDay");
-    var timeBlockEl = $(".container");
+var currentDayEl = $("#currentDay");
+var timeBlockEl = $(".container");
 
-    currentDayEl.text(m.format('MMMM Do YYYY')); // September 11th 2020, 10:46:49 pm
-    timeBlockEl.text(m.format('h:mm:ss a'));
+currentDayEl.text(m.format('MMMM Do YYYY')); // September 11th 2020, 10:46:49 pm
+timeBlockEl.text(m.format('h:mm:ss a'));
 
     
 // moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 10:46:49 pm
