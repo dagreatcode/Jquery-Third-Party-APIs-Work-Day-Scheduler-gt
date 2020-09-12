be4a0563ca5055bc660ef9b28d726e33cbed0235
dagreatcode <dagreatcode@gmail.com>
2020-09-12 00:30:22 -0400

    just testing my console log and cleaning up

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
+// Waits Until The HTML has loaded. Wait4DocReady
 $(document).ready(function () {
     var m = moment();
     console.log(m);
-    // moment().format();
-    // console.log(m.format("dddd MMM Mo YYYY"));
-    // console.log(m.format("[Yeah the year is] dddd [and we're in] YYYY"));
-// //     //Converting the tags
-// var headerJumbotronEl = $(".jumbotron");
+   
+    //Converting the tags
+
+    var headerJumbotronEl = $(".jumbotron");
     var display3El = $(".display-3");
-// var leadEl = $(".lead");
+    var leadEl = $(".lead");
     var currentDayEl = $("#currentDay");
     var timeBlockEl = $(".container");
 
     currentDayEl.text(m.format('MMMM Do YYYY')); // September 11th 2020, 10:46:49 pm
     timeBlockEl.text(m.format('h:mm:ss a'));
-    display3El.ready()
+
 
     console.log(JSON.stringify(m));
  
  });
+                    // Teachers Advice and teachings
+    //  // This line of JavaScript "grabs" the main div on the page ("#drink-options");
+    //  var drinkDiv = document.getElementById("drink-options");
 
+    //  // We then use a for loop to iterate through all the drinks in drinkList.
+    //  // With each iteration, we perform a series of code operations each time.
+    //  for (var i = 0; i < drinkList.length; i++) {
+ 
+    //    // For each drink in the array, we create a new paragraph to hold that text.
+    //    // A new paragraph will be created with each iteration of the loop.
+    //    var newDrinkP = document.createElement("p");
+ 
+    //    // We then assign the the text of this paragraph to be the text in the array.
+    //    newDrinkP.textContent = drinkList[i];
+ 
+    //    // We then add the paragraph to the our main div on the page ("#drink-options")
+    //    drinkDiv.appendChild(newDrinkP);
+    //  }
 
     
 // moment().format('MMMM Do YYYY, h:mm:ss a'); // September 11th 2020, 10:46:49 pm
